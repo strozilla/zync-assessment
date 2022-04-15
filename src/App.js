f7ad3aec4ce0248223310fa9ac7ac926502b8d75
strozilla <74827509+strozilla@users.noreply.github.com>
2022-04-14 19:45:15 -0500

    create search bar and function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useEffect, useState} from 'react';
-import { Row, Col, Container } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 
 
 //components
 import StudentCard from './StudentCard';
-import Search from './Search'
+
 
 import axios from 'axios'
 
@@ -27,21 +27,51 @@ const App = () => {
     
   }, [])
 
+//Search Bar
+ const [searchStudent, setSearchStudent] = useState("")
+  const [filteredStudent, setFilteredStudent] = useState([])
+
+  const searchInput = (searchValue) => {
+    setSearchStudent(searchValue)
+    if(searchStudent !== ''){
+        console.log(searchValue)
+        const findStudent = students.filter((student) => {
+          return Object.values(student).join('').toLowerCase().includes(searchStudent.toLowerCase())
+        })
+        setFilteredStudent(findStudent)
+      console.log(findStudent)
+    } else (
+      setFilteredStudent(students)
+    )
+  }
+
+
   if(!students) return <h3>Loading...</h3>
   return (
     <div className='mx-4'>
-      <Search />
+        <div class="mb-3 ">
+          <label htmlFor="search" className="form-label"></label>
+          <input onChange={(e) => searchInput(e.target.value)} type="text" className="form-control" id="searchBar" placeholder="Find By Name" />
+        </div>
       <Row xs={1} className="mx-auto">
-
-        {students.map((student) => {
-
-          return (
-
-            <Col key={student.id}>
+        {searchStudent.length > 1 ? (
+          filteredStudent.map((student) => {
+            return (
+              <Col key={student.id}>
               <StudentCard student={student}/>
              </Col>  
-           )
-        })}
+            )
+          })
+        ) : (
+            students.map((student) => {
+              return (
+                <Col key={student.id}>
+                <StudentCard student={student}/>
+                </Col>  
+            )
+        })
+          )
+        }
       </Row>
     </div>
   );
